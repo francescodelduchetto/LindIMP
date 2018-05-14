aa9a7aada54c8bed4df93068fad2ac943e8fdaa1
francescodelduchetto <francescodelduchetto@gmail.com>
2018-05-14 15:51:33 +0100

    tmule launch file for startup configuration and simulation ui

--- lindimp_ui/scripts/start_ui.sh
+++ lindimp_ui/scripts/start_ui.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "starting HTTPServer"
-cd ../www
+
+cd $1/www
 
 python -m SimpleHTTPServer
 
-echo "starting Chrome"
-sleep 5
-chrome --fullscreen http://localhost:8000/index.html
+#echo "starting Chrome"
+#sleep 5
+#chrome --fullscreen http://localhost:8000/index.html
 #chrome --kiosk http://localhost:8000/index.html  # not easily possible to minimize chrome without having to close it 
